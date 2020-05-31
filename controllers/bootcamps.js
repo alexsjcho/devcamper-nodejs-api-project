f906b0d22a0e7458f187753ff37db3468664eecc
Alex Cho <alexsjcho@gmail.com>
2020-05-30 21:16:07 -0700

    added POST method, with body parser to create Bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 //@description    Get all bootcamps
 //@route    GET /api/v1/bootcamps
 //@access   Public
@@ -17,8 +19,16 @@ exports.getBootcamp = (req, res, next) => {
 //@description    Create new bootcamp
 //@route    POST /api/v1/bootcamps
 //@access   Public
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Create new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@description    Update bootcamp
