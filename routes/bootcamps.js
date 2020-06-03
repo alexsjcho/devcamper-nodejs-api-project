35b70e82680839cdbc5cfcf9a0b4462484727380
Alex Cho <alexsjcho@gmail.com>
2020-06-02 20:46:22 -0700

    implemented GET bootcamps by distance method function and route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,12 +5,14 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius,
 } = require('../controllers/bootcamps');
 
 const router = express.Router();
 
+//Route Methods
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/').get(getBootcamps).post(createBootcamp);
-
 router
   .route('/:id')
   .get(getBootcamp)
