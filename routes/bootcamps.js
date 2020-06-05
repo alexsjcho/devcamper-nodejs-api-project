20ec3fee755df0f56c7a7c4ba55f9299bc0f7a50
Alex Cho <alexsjcho@gmail.com>
2020-06-04 20:46:59 -0700

    added photo file upload POST method in bootcamps controller

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,6 +6,7 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
 
 // Include other resource routers
@@ -16,7 +17,8 @@ const router = express.Router();
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
-//Route Methods
+//Route methods
+router.route('/:id/photo').put(bootcampPhotoUpload);
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/').get(getBootcamps).post(createBootcamp);
 router
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const fileupload = require('express-fileupload');
 const errorHandler = require('./middleware/errors');
 const connectDB = require('./config/db');
 
@@ -16,14 +18,20 @@ const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 
 const app = express();
-//Body parser
-
-app.use(express.json());
 
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+//Body parser
+app.use(express.json());
+
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
