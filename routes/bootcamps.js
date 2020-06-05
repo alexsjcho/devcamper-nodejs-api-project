ec24c6bdc1dd51e743297a7f7937b409c0b39dcb
Alex Cho <alexsjcho@gmail.com>
2020-06-04 21:04:03 -0700

    added advancedResults middleware function to courses and bootcamps controllers

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,6 +8,9 @@ const {
   getBootcampsInRadius,
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
+const Bootcamp = require('../models/Bootcamp');
+
+const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
 const courseRouter = require('./courses');
@@ -20,7 +23,10 @@ router.use('/:bootcampId/courses', courseRouter);
 //Route methods
 router.route('/:id/photo').put(bootcampPhotoUpload);
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 router
   .route('/:id')
   .get(getBootcamp)
