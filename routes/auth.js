b185f29f56abd1ffa7d5d7dc10b9e1c5f321b60f
Alex Cho <alexsjcho@gmail.com>
2020-06-06 17:44:07 -0700

    added role authorization feature

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { register, login } = require('../controllers/auth');
+const { register, login, getMe } = require('../controllers/auth');
 
+//Import Routes
 const router = express.Router();
+const { protect } = require('../middleware/auth');
 
+//Route paths
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me', protect, getMe);
 
 module.exports = router;
