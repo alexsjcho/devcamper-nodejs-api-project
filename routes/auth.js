bae4e1102bf49bfc6bc348a043928626425e39f5
Alex Cho <alexsjcho@gmail.com>
2020-06-06 19:21:50 -0700

    updated user details feature

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ const {
   login,
   getMe,
   forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controllers/auth');
 
 //Import Routes
@@ -14,6 +17,9 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
 router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
+router.put('/updatepassword', protect, updatePassword);
 
 module.exports = router;
