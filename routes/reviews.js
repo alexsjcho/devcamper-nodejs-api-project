3ea1d8adbbbf6f76a1b502a501dbe83e9535ce4e
Alex Cho <alexsjcho@gmail.com>
2020-06-06 20:06:54 -0700

    added GET Review methods

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { getReviews } = require('../controllers/reviews');
-
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
 const Review = require('../models/Review');
 
 const router = express.Router({ mergeParams: true });
@@ -8,12 +7,15 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').get(
-  advancedResults(Review, {
-    path: 'bootcamp',
-    select: 'name description',
-  }),
-  getReviews
-);
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getReviews
+  )
+  .post(protect, authorize('user', 'admin'), addReview);
 
 module.exports = router;
